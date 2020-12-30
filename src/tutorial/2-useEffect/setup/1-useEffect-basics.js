605b761794ddcaf374cfec758c85022884e661ed
Alyssa Lukens <alyssalukens89@gmail.com>
2020-12-30 11:41:48 -0500

    first commit

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,30 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log('call useEffect');
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    } else {
+      document.title = 'No new messages';
+    }
+  }, [value]);
+
+  useEffect(() => {
+    console.log('Hello World');
+  }, []);
+
+  console.log('render component');
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        click Me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
